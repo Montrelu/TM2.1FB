7de261448867667ecdacef5062739bfefffd8b6c
Noah Montreuil <montrelu@Noahs-MacBook-Air.local>
2023-03-19 20:25:47 -0300

    try routing #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import Login from './components/Login'
+import Register from './components/Register'
+import Questions from './components/Questions'
+import QuestionsT from './components/QuestionsT'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
+const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
 export default function App() {
     return (
-        <p>Hello world</p>
-    )
+      <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<ThemeProvider theme={darkTheme}><CssBaseline/><Login/></ThemeProvider>}/>
+        <Route path="/register" element={<ThemeProvider theme={darkTheme}><CssBaseline/><Register/></ThemeProvider>}/>
+        <Route path="/student" element={<ThemeProvider theme={darkTheme}><CssBaseline/><Questions/></ThemeProvider>}/>
+        <Route path="/login" element={<ThemeProvider theme={darkTheme}><CssBaseline/><QuestionsT/></ThemeProvider>}/>
+      </Routes>
+      </BrowserRouter>  
+    );
 }
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Questions from "./components/Questions"
-import QuestionsT from "./components/QuestionsT"
-import Register from "./components/Register"
-import Login from './components/Login'
+import App from './App'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
@@ -17,4 +14,4 @@ const darkTheme = createTheme({
 
 
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <ThemeProvider theme={darkTheme}><CssBaseline/><Login/></ThemeProvider>);
+  <App/>);
